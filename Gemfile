8161d250c29bc8ca4d4af4dfc277caa9b489d706
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2012-07-02 12:03:20 -0400

    Set up testing

--- Gemfile
+++ Gemfile
@@ -8,7 +8,16 @@ gem 'pg'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'launchy'
+  gem 'database_cleaner'
+end
+
+group :development, :test do
+  gem 'fabrication'
+end
--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,18 @@ end
 
 module Treefinger
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true, views: false
+      g.fixture_replacement :fabrication, dir: "spec/fabricators"
+      g.integration_tool :rspec
+    end
+
     config.encoding = "utf-8"
-
     config.filter_parameters += [:password]
-
     config.active_support.escape_html_entities_in_json = true
-
     config.active_record.whitelist_attributes = true
-
     config.assets.enabled = true
-
     config.assets.version = '1.0'
   end
 end
