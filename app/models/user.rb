80f7d6741943e7b01d7d055d4fa762451ad251a6
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2012-07-15 17:31:03 -0400

    Make username optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,15 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
 
   validates_format_of :email, with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-  validates_format_of :username, with: /[a-z]{2,}/
+  validates_format_of :username, with: /[a-z]{2,}/, unless: :blank_username
 
 
   def self.find_by_email_or_username(login)
     User.where('email = :login OR username = :login', login: login).limit(1).first
+  end
+
+  def blank_username
+    username.blank?
   end
 
 end
