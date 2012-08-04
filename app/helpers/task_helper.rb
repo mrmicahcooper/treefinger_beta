1dde24fbcd2634f96e9d3cf995d0a180eb7c15dd
Micah Cooper <mrmicahcooper@gmail.com>
2012-08-04 17:42:19 -0400

    Show shorter task names when they are long

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -1,9 +1,13 @@
 module TaskHelper
   def task_name(task)
     if task.completed?
-      " - #{task.name} <small> #{right_arrow} #{task.completed_at.strftime('%m/%d/%Y')}  </small>".html_safe
+      " - #{adjusted_task_name(task.name)} <small> #{right_arrow} #{task.completed_at.strftime('%m/%d/%Y')}  </small>".html_safe
     else
-      task.name.html_safe
+      adjusted_task_name(task.name).html_safe
     end
+  end
+
+  def adjusted_task_name(name)
+    name.length > 48 ? name[0..46] + "...." : name
   end
 end
